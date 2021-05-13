5067e8ede9b05bead452b04585ca24795fbb0dfe
aloha-churchill <churchill.aloha@gmail.com>
2021-05-13 15:39:07 -0600

    securing firebase keys

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import firebase from "firebase/app"
 import "firebase/auth"
 import "firebase/database"
 
-/*
+
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREABSE_API_KEY,
   authDomain: process.env.REACT_APP_FIREABSE_AUTH_DOMAIN,
@@ -13,7 +13,7 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
-*/
+/*
 const firebaseConfig = {
   apiKey: "AIzaSyDK1GYXsfADfa5QbSnoTSH5Hw1N3Dp-aHo",
   authDomain: "hamsterwheeltracker.firebaseapp.com",
@@ -24,6 +24,7 @@ const firebaseConfig = {
   appId: "1:395134580216:web:5a1a673fdb20039c96d20d",
   measurementId: "G-CXGTPHS5EV"
 };
+*/
 
 const app = firebase.initializeApp(firebaseConfig);
 
